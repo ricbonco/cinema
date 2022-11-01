0a6e5f4141b72dc85a845c79c0b27cca9d3adf96
Ricardo Bonilla-Cordero <ricbonco@gmail.com>
2022-10-31 23:18:43 -0600

    Adding run_cinema_catalog flow.

--- cinema_catalog/api.py
+++ cinema_catalog/api.py
@@ -3,8 +3,9 @@
 # Import framework
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-import requests, psycopg2, json, os
+import requests, psycopg2, json, os, psutil
 from security import *
+from datetime import datetime
 
 # Instantiate the app
 app = Flask(__name__)
@@ -12,13 +13,16 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 api = Api(app)
 
 security_mode = os.getenv('SECURITYMODE')
+telemetry = os.getenv('TELEMETRY') == 'On'
 
 @app.route('/venues')
 def get_venues():
+    get_telemetry('venues_start')
     conn = psycopg2.connect("host='postgres' dbname='cinema' user='postgres' password='cinema123'")
     try:
         cur = conn.cursor()
 
+        get_telemetry('venues_security_start')
         if security_mode == 'Decentralized':
 
             client_id = request.form.get("client_id")
@@ -47,6 +51,7 @@ def get_venues():
 
             if not "clientId" in data:
                 return jsonify({'success': False, 'details': f'Unauthorized to use this service.'}), 401
+        get_telemetry('venues_security_end')
         
         query = "SELECT v.id as id_venue, v.name, v.location FROM venue AS v"
 
@@ -71,14 +76,17 @@ def get_venues():
         if conn is not None:
             cur.close()
             conn.close()
+        get_telemetry('venues_end')
 
 @app.route('/movies_cinema', methods=["POST"])
 def get_movies_per_cinema():
+    get_telemetry('movies_cinema_start')
     id_venue = request.form.get("id_venue")
     conn = psycopg2.connect("host='postgres' dbname='cinema' user='postgres' password='cinema123'")
     try:
         cur = conn.cursor()
 
+        get_telemetry('movies_cinema_security_start')
         if security_mode == 'Decentralized':
 
             client_id = request.form.get("client_id")
@@ -107,6 +115,7 @@ def get_movies_per_cinema():
 
             if not "clientId" in data:
                 return jsonify({'success': False, 'details': f'Unauthorized to use this service.'}), 401
+        get_telemetry('movies_cinema_security_end')
 
         query = f"""SELECT mv.id as id_movie_venue, m.id as id_movie, m.title, v.id as id_venue, CONCAT(v.name, ' ', v.location) as venue_name
                     FROM movie_venue AS mv
@@ -137,14 +146,17 @@ def get_movies_per_cinema():
         if conn is not None:
             cur.close()
             conn.close()
+        get_telemetry('movies_cinema_end')
 
 @app.route('/movie_times_cinema', methods=["POST"])
 def get_movie_times_per_cinema():
+    get_telemetry('movie_times_cinema_start')
     id_movie_venue = request.form.get("id_movie_venue")
     conn = psycopg2.connect("host='postgres' dbname='cinema' user='postgres' password='cinema123'")
     try:
         cur = conn.cursor()
 
+        get_telemetry('movie_times_cinema_security_start')
         if security_mode == 'Decentralized':
 
             client_id = request.form.get("client_id")
@@ -173,6 +185,7 @@ def get_movie_times_per_cinema():
 
             if not "clientId" in data:
                 return jsonify({'success': False, 'details': f'Unauthorized to use this service.'}), 401
+        get_telemetry('movie_times_cinema_security_end')
 
         query = f"""SELECT mt.id as id_movies_times, m.title, CONCAT(v.name, ' ', v.location) as venue, mt.movie_date
                    FROM movie_time as mt
@@ -205,14 +218,17 @@ def get_movie_times_per_cinema():
         if conn is not None:
             cur.close()
             conn.close()
+        get_telemetry('movie_times_cinema_end')
 
 @app.route('/movie_seats_venue_times', methods=["POST"])
 def get_movie_seats_per_venue_and_times():
+    get_telemetry('movie_seats_venue_times_start')
     id_movie_time = request.form.get("id_movie_time")
     conn = psycopg2.connect("host='postgres' dbname='cinema' user='postgres' password='cinema123'")
     try:
         cur = conn.cursor()
 
+        get_telemetry('movie_seats_venue_times_security_start')
         if security_mode == 'Decentralized':
 
             client_id = request.form.get("client_id")
@@ -241,6 +257,7 @@ def get_movie_seats_per_venue_and_times():
 
             if not "clientId" in data:
                 return jsonify({'success': False, 'details': f'Unauthorized to use this service.'}), 401
+        get_telemetry('movie_seats_venue_times_security_end')
 
         query = f"""SELECT ms.id as id_movie_seat, ms.id_seat_type, m.title, CONCAT(v.name, ' ', v.location) as venue, ms.total_seats, ms.available_seats, ms.price::numeric::float, cv.value as ticket_type 
                    FROM movie_seat as ms
@@ -277,6 +294,18 @@ def get_movie_seats_per_venue_and_times():
         if conn is not None:
             cur.close()
             conn.close()
+        get_telemetry('movie_seats_venue_times_end')
+
+def get_telemetry(operation):
+    if telemetry:
+        cpu_usage = psutil.cpu_percent()
+        ram_usage = psutil.virtual_memory().percent
+        log(f"{datetime.now()},{operation},{cpu_usage},{ram_usage}")
+
+def log(text):
+    file = open("cinema_catalog.csv", "a")  
+    file.write(f"{text}\n")
+    file.close()   
 
 # Run the application
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -1,29 +1,71 @@
-import os, shutil, requests, psutil, json
+import os, shutil, requests, psutil, json, random
 from datetime import datetime 
 
 def run_movies_flow(cycles):
     print("Running 'run_movies_flow'")
     for i in range(cycles):
-        get_telemetry('movies_start')
+        get_telemetry('movies_flow_start')
 
+        print(f"Cycle #{i}")
+
+        # Calling movies
+        print(" Running 'movies' service")  
         url = "http://localhost:8081/movies"
         body = {'client_id' : client_id, 'client_secret' : client_secret}
         r = requests.get(url, data = body, headers = header)
-        print_response(r)
+        print_response(url, r)
 
-        get_telemetry('movies_end')
+        get_telemetry('movies_flow_end')
 
 def run_cinema_catalog_flow(cycles):    
     print("Running 'run_cinema_catalog_flow'")  
     for i in range(cycles):
-        get_telemetry('venues_start')
+        get_telemetry('cinema_catalog_flow_start')
 
+        print(f"Cycle #{i}")
+
+        # Calling venues
+        print(" Running 'venues' service")  
         url = "http://localhost:8082/venues"
         body = {'client_id' : client_id, 'client_secret' : client_secret}
         r = requests.get(url, data = body, headers = header)
-        print_response(r)
+        print_response(url, r)
+        data = json.loads(r.text)
+        venues_count = len(data["venues"])
+        index = random.randint(0, venues_count-1)
+        id_venue = data["venues"][index]["id_venue"]
 
-        get_telemetry('venues_end')
+        # Calling movies_cinema
+        print(" Running 'movies_cinema' service")  
+        url = "http://localhost:8082/movies_cinema"
+        body = {'client_id' : client_id, 'client_secret' : client_secret, 'id_venue' : id_venue}
+        r = requests.post(url, data = body, headers = header)
+        print_response(url, r)
+        data = json.loads(r.text)
+        movies_by_venues_count = len(data["movies_by_venues"])
+        index = random.randint(0, movies_by_venues_count-1)
+        id_movie_venue = data["movies_by_venues"][index]["id_movie_venue"]
+        
+        # Calling movie_times_cinema
+        print(" Running 'movie_times_cinema' service")  
+        url = "http://localhost:8082/movie_times_cinema"
+        body = {'client_id' : client_id, 'client_secret' : client_secret, 'id_movie_venue' : id_movie_venue}
+        r = requests.post(url, data = body, headers = header)
+        print_response(url, r)
+        data = json.loads(r.text)
+        movie_times_cinema_count = len(data["movie_times_cinema"])
+        index = random.randint(0, movie_times_cinema_count-1)
+        id_movie_time = data["movie_times_cinema"][index]["id_movies_times"]
+
+        # Calling movie_seats_venue_times
+        print(" Running 'movie_seats_venue_times' service")  
+        url = "http://localhost:8082/movie_seats_venue_times"
+        body = {'client_id' : client_id, 'client_secret' : client_secret, 'id_movie_time' : id_movie_time}
+        r = requests.post(url, data = body, headers = header)
+        print_response(url, r)
+        data = json.loads(r.text)
+        
+        get_telemetry('cinema_catalog_flow_end')
 
 def copy_logs():
     now = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
@@ -33,10 +75,14 @@ def copy_logs():
     os.mkdir(logs_directory)
     if os.path.exists("log.csv"):
         shutil.move("log.csv", f"{logs_directory}/log.csv")
-    shutil.move("movies/movies.csv", f"{logs_directory}/movies.csv")
+    if os.path.exists("movies/movies.csv"):
+        shutil.move("movies/movies.csv", f"{logs_directory}/movies.csv")
+    if os.path.exists("cinema_catalog/cinema_catalog.csv"):        
+        shutil.move("cinema_catalog/cinema_catalog.csv", f"{logs_directory}/cinema_catalog.csv")
+    
 
-def print_response(response):
-    print(f"  Status Code: {response.status_code}\n    Output: {response.text}")
+def print_response(url, response):
+    print(f"  URL: {url}\n    Status Code: {response.status_code}\n      Output: {response.text}")
 
 def get_telemetry(operation):
     if telemetry:
@@ -54,7 +100,7 @@ def authenticate(client_id, client_secret):
     url = "http://localhost:8080/auth"
     body = {'client_id' : client_id, 'client_secret' : client_secret}
     r = requests.post(url, data = body)
-    print_response(r)
+    print_response(url, r)
     data = json.loads(r.text)
     if "token" in data:
         return data["token"]
@@ -62,12 +108,12 @@ def authenticate(client_id, client_secret):
         print(f"Error when authenticating '{client_id}'")
         return False
 
-def set_up():
+def set_up(print_telemetry, user_id, user_password):
     global telemetry, client_id, client_secret, header
 
-    telemetry = True
-    client_id = 'ricardo'
-    client_secret = 'ricardo1234'
+    telemetry = print_telemetry
+    client_id = user_id
+    client_secret = user_password
 
     token = authenticate(client_id, client_secret)
 
@@ -75,7 +121,7 @@ def set_up():
 
 def main():
     
-    set_up()
+    set_up(True, 'ricardo', 'ricardo1234')
 
     get_telemetry('test_start')
 
