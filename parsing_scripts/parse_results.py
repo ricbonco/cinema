2be34e149f57945a950d01ad7804c73d099e4577
ricbonco <102190078+ricbonco@users.noreply.github.com>
2022-12-14 21:06:08 -0600

    Enhancing Parsing Script and Cleaning Up Code (#21)
    * Adding initial code.
    
    * Removing comments.
    
    * Using specific Postgres version to avoid issues with interaction between app and database.
    
    * Added new methods and database tables.
    
    * Add cinema and bookings services.
    
    * Renamed data base entities to singular.
    
    * Add basic payment service.
    
    * Notification service first implementation.
    
    * Notify payment code.
    
    * Integrating authentication into services and first implementation of reports service.
    
    * Adding employee role to security service.
    
    * Adding local security to services.
    
    * Fix issues with authentication.
    
    * Adding further telemetry and first run.py script.
    
    * Adding run_cinema_catalog flow.
    
    * Adding booking flow.
    
    * Adding payment flow.
    
    * Add reports flow and notification service telemetry.
    
    * Adding new flows.
    
    * Fixed notifications service call from payment service and code cleanup
    
    * Adding roles and refining experiment flows.
    
    * Adding parsing script.
    
    * Changes to parsing script.
    
    * Changed parsing script.
    
    * Enhancing parsing script.

--- parsing_scripts/parse_results.py
+++ parsing_scripts/parse_results.py
@@ -6,23 +6,21 @@ import sys, os, shutil
 # Create a Pandas Excel writer using XlsxWriter as the engine.
 writer = pd.ExcelWriter('Summary.xlsx', engine='xlsxwriter')
 
-#if len(sys.argv) == 3:
+# Read directory location
 directory = sys.argv[1] 
-#N = int(sys.argv[2])
-#N = 10
-#else:
-#    directory = os.getcwd()
 
+# Getting input .xlsx files
 files = os.listdir(directory)
 files_to_process = [f'{directory}/{f}' for f in files if os.path.isfile(directory+'/'+f) and ".xlsx" in f] #Filtering only the files.
-# Ricardo print(files_to_process)
 
 for file in files_to_process:
+    # Column definition for the three measurements
     df = pd.DataFrame(columns = ['Operation', 'Min', 'Max', 'Mean', 'Std', 'Variance', 'CoV'])
     df_cpu_final = pd.DataFrame(columns = ['Operation', 'Min', 'Max', 'Mean', 'Std', 'Variance', 'CoV'])
     df_ram_final = pd.DataFrame(columns = ['Operation', 'Min', 'Max', 'Mean', 'Std', 'Variance', 'CoV'])
     excel_data_df = pd.read_excel(file, sheet_name='Sheet1')
 
+    # Getting measuring points names
     operations_list = list(dict.fromkeys(excel_data_df['Operation'].tolist()))
     measurements_list = []
     for element in operations_list:
@@ -30,12 +28,9 @@ for file in files_to_process:
 
     measurements_list = list(dict.fromkeys(measurements_list))
 
-    # Ricardo print(f'measurements_list {measurements_list}')
-
     for measurement in measurements_list:
 
-        # Ricardo print(f"*** measurement {measurement}")
-
+        # Getting start and end times
         df_start = excel_data_df.loc[excel_data_df['Operation'] == f"{measurement}_start"]
         df_end = excel_data_df.loc[excel_data_df['Operation'] == f"{measurement}_end"]
 
@@ -49,11 +44,11 @@ for file in files_to_process:
 
         ram_list = df_start['RAM'].tolist() + df_end['RAM'].tolist()
 
-        # Ricardo print(f'len start {len(df_start_list)} len end {len(df_end_list)} ')
-
+        # Getting total execution time for operation
         for i in range(len(df_time_start_list)):
             time_list.append(pd.to_datetime(df_time_end_list[i]) - pd.to_datetime(df_time_start_list[i]))
 
+        # Preprocessing duration for getting statistical data
         df_duration = pd.DataFrame (time_list, columns = ['Duration'])
 
         df_duration['new'] = df_duration['Duration'].values.astype(np.int64)
@@ -62,22 +57,18 @@ for file in files_to_process:
         duration_set_list = duration_set.tolist()
         duration_set_list.sort()
 
-        #cpu_list.sort()
-
         N = int(round(len(duration_set_list) * 0.01, 0) / 2) 
-        print (f'N: {N}')
 
         del duration_set_list[:N]
         del duration_set_list[-N:]
-
-        #del cpu_list[:N]
-        #del cpu_list[-N:]
 
         duration_set = pd.Series(duration_set_list)
 
         series_cpu = pd.Series(cpu_list)
 
         series_ram = pd.Series(ram_list)
+
+        # Summarizing information
 
         # Time
         minimum_time = pd.to_timedelta(np.min(duration_set))
@@ -87,8 +78,7 @@ for file in files_to_process:
         variance_time = duration_set.var(ddof=0)
         coefficient_of_variance_time = std_time / average_time
 
-        df = df.append({'Operation' : measurement, 'Min' : minimum_time, 'Max' : maximum_time, 'Mean' : average_time, 'Std': std_time, 'Variance' : variance_time, 'CoV': coefficient_of_variance_time},
-            ignore_index = True)
+        df = pd.concat([df, pd.DataFrame({'Operation' : measurement, 'Min' : minimum_time, 'Max' : maximum_time, 'Mean' : average_time, 'Std': std_time, 'Variance' : variance_time, 'CoV': coefficient_of_variance_time}, index=[0])])
 
         # CPU Usage
         minimum_cpu = pd.to_numeric(np.min(series_cpu))
@@ -98,8 +88,7 @@ for file in files_to_process:
         variance_cpu = series_cpu.var(ddof=0)
         coefficient_of_variance_cpu = std_cpu / average_cpu
 
-        df_cpu_final = df_cpu_final.append({'Operation' : measurement, 'Min' : minimum_cpu, 'Max' : maximum_cpu, 'Mean' : average_cpu, 'Std': std_cpu, 'Variance' : variance_cpu, 'CoV': coefficient_of_variance_cpu},
-            ignore_index = True)
+        df_cpu_final = pd.concat([df_cpu_final, pd.DataFrame({'Operation' : measurement, 'Min' : minimum_cpu, 'Max' : maximum_cpu, 'Mean' : average_cpu, 'Std': std_cpu, 'Variance' : variance_cpu, 'CoV': coefficient_of_variance_cpu}, index=[0])])
 
         # RAM Usage
         minimum_ram = pd.to_numeric(np.min(series_ram))
@@ -109,13 +98,11 @@ for file in files_to_process:
         variance_ram = series_ram.var(ddof=0)
         coefficient_of_variance_ram = std_ram / average_ram
 
-        df_ram_final = df_ram_final.append({'Operation' : measurement, 'Min' : minimum_ram, 'Max' : maximum_ram, 'Mean' : average_ram, 'Std': std_ram, 'Variance' : variance_ram, 'CoV': coefficient_of_variance_ram},
-            ignore_index = True)
+        df_ram_final = pd.concat([df_ram_final, pd.DataFrame({'Operation' : measurement, 'Min' : minimum_ram, 'Max' : maximum_ram, 'Mean' : average_ram, 'Std': std_ram, 'Variance' : variance_ram, 'CoV': coefficient_of_variance_ram}, index=[0])])
             
         sheet_time = f'{file.split("/")[-1].replace(".xlsx", "")}_time'
         sheet_cpu = f'{file.split("/")[-1].replace(".xlsx", "")}_cpu'
         sheet_ram = f'{file.split("/")[-1].replace(".xlsx", "")}_ram'
-        # Ricardo print(sheet)
         # Convert the dataframe to an XlsxWriter Excel object.
         df.to_excel(writer, sheet_name=sheet_time, index=False)
         df_cpu_final.to_excel(writer, sheet_name=sheet_cpu, index=False)
